016616b7b34fad7e2d30b899210b5c24d1357acf
Emiliya <emilya.korkunova@gmail.com>
2022-10-16 00:50:13 +0300

    add 5

--- src/what-season.js
+++ src/what-season.js
@@ -13,13 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
  function getSeason(date) {
   if (!date) return ('Unable to determine the time of year!');
-  if (Object.getPrototypeOf(date) !== Object.getPrototypeOf(new Date())) return ('Invalid date!');
+  if (date.__proto__ !== new Date().__proto__  || typeof date !== 'object'
+  || Object.prototype.toString.call(date) !== '[object Date]') throw new Error('Invalid date!');
+  try {
+    date.toLocaleString()
+  } catch (e) {
+    throw new Error('Invalid date!')
+  }
   const seasons = [['winter', 0, 1, 11], ['spring', 2, 3, 4], ['summer', 5, 6, 7], ['autumn', 8, 9, 10]];
    const month = date.getMonth();
- console.log(month)
+
  for (let i = 0; i < seasons.length; i++) {
-  if (seasons[i].includes(month)) return seasons[i][0];
- }}
+  if (seasons[i].includes(month)) {
+    return seasons[i][0];
+  }
+
+}}
 
 module.exports = {
   getSeason
